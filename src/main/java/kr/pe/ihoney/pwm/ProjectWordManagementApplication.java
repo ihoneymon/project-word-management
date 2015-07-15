d87bbb9a3c035aa9d6589c0788ccfb35a5c83b4d
ihoneymon <ihoneymon@gmail.com>
2015-07-15 15:26:12 +0900

    Init Project

---
+++ src/main/java/kr/pe/ihoney/pwm/ProjectWordManagementApplication.java
@@ -0,0 +1,19 @@
+package kr.pe.ihoney.pwm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 프로젝트 용어 관리 프로젝트
+ *
+ * @author jiheon
+ * @since 2015/07/15
+ *
+ */
+@SpringBootApplication
+public class ProjectWordManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWordManagementApplication.class, args);
+    }
+}
